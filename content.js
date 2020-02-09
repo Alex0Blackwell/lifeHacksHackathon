3ca6196cb1c81532bf59c403a5355bc315ec8476
alex blackwell <alex.blackwell314@gmail.com>
2020-02-08 17:27:30 -0800

    Website styled and optimized

--- content.js
+++
@@ -1,9 +0,0 @@
-console.log("ok linked");
-
-chrome.runtime.onMessage.addListener(gotMessage);
-
-function gotMessage(message, sender, sendResponse) {
-    console.log(message.txt);
-}
-
-console.log(window.location.toString());
